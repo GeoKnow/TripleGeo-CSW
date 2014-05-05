9d191f595cd18a0b8cc9211364b6a12433fed53f
ngeorgomanolis <ngeorgomanolis@gmail.com>
2014-05-05 18:21:53 +0200

    script updated

--- search.py
+++ search.py
@@ -7,13 +7,22 @@ import urllib
 from lxml import etree
 import StringIO
 import ConfigParser as configparser
+import tempfile
+import logging
+
 
 # Globals
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Functions 
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stderr)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+# Functions
+
 
 def createAll(list):
     s = 'GetRecords.xml'
@@ -124,14 +133,15 @@ def createXmlLike(dictionary, box):
 ##################################################
 
 
-def transform(i):
-    parser = etree.XMLParser(recover=True)
-    dom = etree.parse('out' + str(i) + '.xml', parser)
+def transform(temp):
+    temp.seek(0)
+    xml=temp.read()
+    parser=etree.XMLParser(recover=True)
+    dom = etree.fromstring(xml,parser)
     xslt = etree.parse('Metadata2RDF.xsl')
     transform = etree.XSLT(xslt)
     newdom = transform(dom)
-    rdf = open('out' + str(i) + '.rdf', 'w+')
-    rdf.write(etree.tostring(newdom, pretty_print=True))
+    sys.stdout.write(etree.tostring(newdom, pretty_print=True))
 
 ###########################
 #function to POST Requests#
@@ -140,27 +150,26 @@ def transform(i):
 
 def request(s):
     csw_endpoints = config.get('main', 'csw_endpoints', '').split()
-    i = 1
     for j in csw_endpoints:
-        file = open('out' + str(i) + '.xml', 'w')
-        url = j
-        payload = open(str(s)).read()
-        headers = {"Content-type": "application/xml", "Accept": "text/plain"}
-        try:
-            r = urllib2.Request(url, payload, headers)
-            u = urllib2.urlopen(r)
-            xml_response = u.read()
-            # print xml_response
-            if '<gmd:MD_Metadata' in xml_response:
-                file = open('out' + str(i) + '.xml', 'w')
-                file.write(xml_response)
-                transform(i)
-                i = i + 1
-                print 'Response OK:', j, file
-            else:
-                print 'Response nothing:', j
-        except urllib2.URLError, (err):
-            print "URL error(%s)" % (err)
+        with tempfile.NamedTemporaryFile() as temp:
+            url = j
+            payload = open(str(s)).read()
+            headers = {"Content-type": "application/xml", "Accept": "text/plain"}
+            try:
+                r = urllib2.Request(url, payload, headers)
+                u = urllib2.urlopen(r)
+                xml_response = u.read()
+                if '<gmd:MD_Metadata' in xml_response:
+                    temp.write(xml_response)
+                    transform(temp)
+                    logger.info('Response OK:' +j)
+                    #print 'Response OK:', j, file
+                else:
+                    logger.info('Response nothing:' +j)
+                    #print 'Response nothing:', j
+            except urllib2.URLError as xxx_todo_changeme:
+                (err) = xxx_todo_changeme
+                #print "URL error(%s)" % (err)
     clear()
 
 ###########################################################################
@@ -194,95 +203,94 @@ def extract_filter(query_file):
             flt = s.group()
             return flt
 
-# Main 
+# Main
+
 
 def main(query_file):
-    # Todo ...
-    pass
+    list = []
+    dic = {}
+    box = []
+    with open(query_file, 'r') as f:
+        for line in f:
+            if re.search('([a-z]):([a-z])', line) and 'SELECT' not in line and str(extract_filter(query_file)) not in line:
+                if '{' and '}' in line:
+                    s = line[line.find('{') + 1:line.find('}')]
+                    rgx1 = re.compile('([\w+\?-]*\w)')
+                    q = rgx1.findall(s)
+                    chunks = [q[x:x + 4] for x in xrange(0, len(q), 4)]
+                    y = [dic.values()]
+                    for q in chunks:
+                        if '?' in q[0] and '?' not in q[3]:
+                            k = q[1] + ':' + q[2]
+                            if k in dic.keys():
+                                dic[k].append(q[3])
+                            else:
+                                dic[q[1] + ':' + q[2]] = [q[3]]
 
+                        else:
+                            list.append(q)
 
-####################~START~########################
+                if '{' and ':' in line and '}' not in line:
+                    s = line[line.find('{') + 1:line.find('}')]
+                    rgx1 = re.compile('([\w+\?-]*\w)')
+                    q = rgx1.findall(s)
+                    chunks = [q[x:x + 4] for x in xrange(0, len(q), 4)]
+                    y = [dic.values()]
+                    for q in chunks:
+                        if '?'in q[0] and '?' not in q[3]:
+                            k = q[1] + ':' + q[2]
+                            if k in dic.keys():
+                                dic[k].append(q[3])
+                            else:
+                                dic[q[1] + ':' + q[2]] = [q[3]]
 
+                        else:
+                            list.append(q)
+
+            if re.search('regex', line, re.IGNORECASE):
+                s1 = re.search('regex', line, re.IGNORECASE)
+                reg = s1.group()
+                s = line[line.find(str(reg)):]
+                if '&&' in s:
+                    sf = line[line.find(str(reg)):line.rfind('&&')]
+                    rgx1 = re.compile('([\w+\^?-]*\w)')
+                    q = rgx1.findall(sf)
+                    z = 0
+                    for j in list:
+                        for i in q:
+                            if i in j[3]:
+                                dic.update(
+                                    {j[1] + ':' + j[2]: ['*' + q[len(q) - 1] + '*']})
+                        z = z + 1
+                else:
+                    sf = line[line.find(str(reg)):line.rfind('}')]
+                    rgx1 = re.compile('([\w+\^?-]*\w)')
+                    q = rgx1.findall(sf)
+                    z = 0
+                    for j in list:
+                        for i in q:
+                            if i in j[3]:
+                                dic.update(
+                                    {j[1] + ':' + j[2]: ['*' + q[len(q) - 1] + '*']})
+                        z = z + 1
+
+            if re.search('box2d', line, re.IGNORECASE):
+                s = re.search('box2d', line, re.IGNORECASE)
+                bo = s.group()
+                s = line[line.find(bo):]
+                box = s.split('(', 1)[1].split(')')[0].split(',')
+
+    if len(dic) == 0 and len(box) == 0:
+        #print 'dictionary empty: nothing to Search.. '
+        #print 'Searching all available metadata with AnyText..acoording to given predicates.'
+        #print list
+        createAll(list)
+    else:
+        #print dic, list, box
+        createXmlLike(dic, box)
+        pass
+
+#~START~#
+#read query 
 query_file = sys.argv[1]
-
-list = []
-dic = {}
-box = []
-with open(query_file, 'r') as f:
-    for line in f:
-        if re.search('([a-z]):([a-z])', line) and 'SELECT' not in line and str(extract_filter(query_file)) not in line:
-            if '{' and '}' in line:
-                s = line[line.find('{') + 1:line.find('}')]
-                rgx1 = re.compile('([\w+\?-]*\w)')
-                q = rgx1.findall(s)
-                chunks = [q[x:x + 4] for x in xrange(0, len(q), 4)]
-                y = [dic.values()]
-                for q in chunks:
-                    if '?' in q[0] and '?' not in q[3]:
-                        k = q[1] + ':' + q[2]
-                        if k in dic.keys():
-                            dic[k].append(q[3])
-                        else:
-                            dic[q[1] + ':' + q[2]] = [q[3]]
-
-                    else:
-                        list.append(q)
-
-            if '{' and ':' in line and '}' not in line:
-                s = line[line.find('{') + 1:line.find('}')]
-                rgx1 = re.compile('([\w+\?-]*\w)')
-                q = rgx1.findall(s)
-                chunks = [q[x:x + 4] for x in xrange(0, len(q), 4)]
-                y = [dic.values()]
-                for q in chunks:
-                    if '?'in q[0] and '?' not in q[3]:
-                        k = q[1] + ':' + q[2]
-                        if k in dic.keys():
-                            dic[k].append(q[3])
-                        else:
-                            dic[q[1] + ':' + q[2]] = [q[3]]
-
-                    else:
-                        list.append(q)
-
-        if re.search('regex', line, re.IGNORECASE):
-            s1 = re.search('regex', line, re.IGNORECASE)
-            reg = s1.group()
-            s = line[line.find(str(reg)):]
-            if '&&' in s:
-                sf = line[line.find(str(reg)):line.rfind('&&')]
-                rgx1 = re.compile('([\w+\^?-]*\w)')
-                q = rgx1.findall(sf)
-                z = 0
-                for j in list:
-                    for i in q:
-                        if i in j[3]:
-                            dic.update(
-                                {j[1] + ':' + j[2]: ['*' + q[len(q) - 1] + '*']})
-                    z = z + 1
-            else:
-                sf = line[line.find(str(reg)):line.rfind('}')]
-                rgx1 = re.compile('([\w+\^?-]*\w)')
-                q = rgx1.findall(sf)
-                z = 0
-                for j in list:
-                    for i in q:
-                        if i in j[3]:
-                            dic.update(
-                                {j[1] + ':' + j[2]: ['*' + q[len(q) - 1] + '*']})
-                    z = z + 1
-
-        if re.search('box2d', line, re.IGNORECASE):
-            s = re.search('box2d', line, re.IGNORECASE)
-            bo = s.group()
-            s = line[line.find(bo):]
-            box = s.split('(', 1)[1].split(')')[0].split(',')
-
-if len(dic) == 0 and len(box) == 0:
-    print 'dictionary empty: nothing to Search.. '
-    print 'Searching all available metadata with AnyText..acoording to given predicates.'
-    print list
-    createAll(list)
-else:
-    print dic, list, box
-    createXmlLike(dic, box)
+main(query_file)
